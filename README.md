5eb5a6cd03b2f9cf2954be93388c762e4b596973
zhongsheng <191492580@qq.com>
2018-03-30 10:02:43 +0800

    增加了滚动方向参数

--- README.md
+++ README.md
@@ -60,4 +60,8 @@ class MarqueePage extends Component {
 Api
 ----
 
-![api](./ass/api.png)
+| 参数 | 说明 | 类型 | 默认值 |
+| :--- | :----: | ----: | ----：|
+| loopData | 滚动的数据 | Array | 无 |
+| getMarquee | 可以从参数中获取stop，run函数控制marque的运动和暂停 | function | 无 |
+| direction | 滚动方向 | String ('landscape' || 'vertical') | 'landscape' |
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as utils from '../utils/raf'
-import './index.css';
+import './marquee_landscape.css';
+import './marquee_vertical.css';
 
 utils.raf();
 
 export default class Marquee extends Component {
     static defaultProps = {
-        loop: false
+        direction: 'landscape',
+        verticalItemHeight: '60px'
     }
 
     constructor(props) {
@@ -20,20 +22,31 @@ export default class Marquee extends Component {
         this.state = {};
     }
 
-    marquee = () => {
-        this.domMw.scrollLeft >= this.domMi.scrollWidth ? this.domMw.scrollLeft = 0 : this.domMw.scrollLeft++;
-        this.timerMarquee = requestAnimationFrame(this.marquee);
-    }
-
     initMarquee = () => {
         this.timerMarquee && this.stopMarquee();
         this.runMarquee();
     }
 
+    //横向滚动
+    landscapeMarquee = () => {
+        this.domMw.scrollLeft >= this.domMi.scrollWidth ? this.domMw.scrollLeft = 0 : this.domMw.scrollLeft++;
+        this.timerMarquee = requestAnimationFrame(this.landscapeMarquee);
+    }
+
+    //竖向滚动
+    verticalMarquee = () => {
+        this.domMw.scrollTop >= this.domMi.scrollHeight ? this.domMw.scrollTop = 0 : this.domMw.scrollTop++;
+        this.timerMarquee = requestAnimationFrame(this.verticalMarquee);
+    }
+
     // 运动
     runMarquee = () => {
         this.timerMarquee && this.stopMarquee();
-        this.timerMarquee = requestAnimationFrame(this.marquee);
+        if (this.props.direction === 'vertical') {
+            this.timerMarquee = requestAnimationFrame(this.verticalMarquee);
+        } else {
+            this.timerMarquee = requestAnimationFrame(this.landscapeMarquee);
+        }
     }
 
     //暂停
@@ -44,31 +57,52 @@ export default class Marquee extends Component {
     componentDidMount = () => {
         this.initMarquee();
 
-        let {getMarquee} = this.props
+        let { getMarquee } = this.props
 
         getMarquee && getMarquee({
             runMarquee: this.runMarquee,
             stopMarquee: this.stopMarquee
         });
-      
+
+    }
+
+    renderLandscapeMarquee() {
+        let { loopData } = this.props;
+
+        return (
+            <div className="marquee-landscape-wrap" ref={(mw) => { this.domMw = mw; }}>
+                <div className="marquee-landscape-item" ref={(mi) => { this.domMi = mi; }}>
+                    {loopData.map((item, index) => (<div className="marquee-landscape-txt" key={index}>{item.txt}</div>))}
+                </div>
+                <div className="marquee-landscape-item">
+                    {loopData.map((item, index) => (<div className="marquee-landscape-txt" key={index}>{item.txt}</div>))}
+                </div>
+            </div>
+        )
+    }
+
+    renderVerticalMarquee() {
+        let { loopData,verticalItemHeight } = this.props;
+        return (
+            <div className="marquee-vertical-wrap" ref={(mw) => { this.domMw = mw; }}>
+                <div className="marquee-vertical-item" ref={(mi) => { this.domMi = mi; }}>
+                    {loopData.map((item, index) => (<div style={{height:verticalItemHeight, lineHeight:verticalItemHeight}} className="marquee-vertical-txt" key={index}>{item.txt}</div>))}
+                </div>
+                <div className="marquee-vertical-item">
+                    {loopData.map((item, index) => (<div style={{height:verticalItemHeight, lineHeight:verticalItemHeight}} className="marquee-vertical-txt" key={index}>{item.txt}</div>))}
+                </div>
+            </div>
+        )
     }
 
     render() {
 
-        let { loop, loopData } = this.props;
-        let { marqueeWidth } = this.state;
+        let { direction } = this.props;
 
         return (
             <div className="react-marquee-box">
-                <div className="marquee-wrap" ref={(mw) => { this.domMw = mw; }}>
-                    <div className="marquee-item" ref={(mi) => { this.domMi = mi; }}>
-                        {loopData.map((item, index) => (<div className="marquee-txt" key={index}>{item.txt}</div>))}
-                    </div>
-                    <div className="marquee-item">
-                        {loopData.map((item, index) => (<div className="marquee-txt" key={index}>{item.txt}</div>))}
-                    </div>
-                </div>
-            </div >
+                {direction === 'landscape' ? this.renderLandscapeMarquee() : this.renderVerticalMarquee()}
+            </div>
         )
     }
 }
@@ -76,5 +110,7 @@ export default class Marquee extends Component {
 Marquee.propTypes = {
     loop: PropTypes.bool,
     loopData: PropTypes.array,
-    getMarquee: PropTypes.func
+    getMarquee: PropTypes.func,
+    direction: PropTypes.string,
+    verticalItemHeight: PropTypes.string
 };
