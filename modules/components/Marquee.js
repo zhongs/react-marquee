2c6f14f72c86c134d26b923d7a06b3012a9f5d27
zhongs <191492580@qq.com>
2024-12-09 10:21:34 +0800

    feat: 优化组件API
    1. 使用 ref 替代 getMarquee 回调
    2. 添加 autoPlay 配置控制初始化是否自动开始滚动
    3. 添加 onClick 回调支持点击事件
    4. 优化 hoverPause 功能，添加鼠标手型指针

--- modules/components/Marquee.js
+++ modules/components/Marquee.js
@@ -8,7 +8,12 @@ utils.raf();
 export default class Marquee extends Component {
   static defaultProps = {
     direction: 'landscape',
-    verticalItemHeight: '60px'
+    verticalItemHeight: '60px',
+    speed: 2,
+    reverse: false,
+    hoverPause: false,
+    autoPlay: true,
+    onClick: null
   }
 
   constructor(props) {
@@ -23,18 +28,50 @@ export default class Marquee extends Component {
 
   initMarquee = () => {
     this.stopMarquee();
-    this.runMarquee();
+    if (this.props.autoPlay) {
+      this.runMarquee();
+    }
   }
 
   //横向滚动
   landscapeMarquee = () => {
-    this.domMw.scrollLeft >= this.domMi.scrollWidth ? this.domMw.scrollLeft = 0 : this.domMw.scrollLeft++;
+    const { speed, reverse } = this.props;
+    if (reverse) {
+      // 反向滚动
+      if (this.domMw.scrollLeft <= 0) {
+        this.domMw.scrollLeft = this.domMi.scrollWidth;
+      } else {
+        this.domMw.scrollLeft -= speed;
+      }
+    } else {
+      // 正向滚动
+      if (this.domMw.scrollLeft >= this.domMi.scrollWidth) {
+        this.domMw.scrollLeft = 0;
+      } else {
+        this.domMw.scrollLeft += speed;
+      }
+    }
     this.timerMarquee = requestAnimationFrame(this.landscapeMarquee);
   }
 
   //竖向滚动
   verticalMarquee = () => {
-    this.domMw.scrollTop >= this.domMi.scrollHeight ? this.domMw.scrollTop = 0 : this.domMw.scrollTop++;
+    const { speed, reverse } = this.props;
+    if (reverse) {
+      // 反向滚动
+      if (this.domMw.scrollTop <= 0) {
+        this.domMw.scrollTop = this.domMi.scrollHeight;
+      } else {
+        this.domMw.scrollTop -= speed;
+      }
+    } else {
+      // 正向滚动
+      if (this.domMw.scrollTop >= this.domMi.scrollHeight) {
+        this.domMw.scrollTop = 0;
+      } else {
+        this.domMw.scrollTop += speed;
+      }
+    }
     this.timerMarquee = requestAnimationFrame(this.verticalMarquee);
   }
 
@@ -53,53 +90,116 @@ export default class Marquee extends Component {
     this.timerMarquee && cancelAnimationFrame(this.timerMarquee)
   }
 
+  handleMouseEnter = () => {
+    if (this.props.hoverPause) {
+      this.stop();
+    }
+  }
+
+  handleMouseLeave = () => {
+    if (this.props.hoverPause) {
+      this.start();
+    }
+  }
+
+  handleItemClick = (item, index) => {
+    const { onClick } = this.props;
+    if (onClick) {
+      onClick(item, index);
+    }
+  }
+
   componentDidMount = () => {
     this.initMarquee();
-
-    let { getMarquee } = this.props
-
-    getMarquee && getMarquee({
-      runMarquee: this.runMarquee,
-      stopMarquee: this.stopMarquee
-    });
-
+    window.addEventListener('resize', this.handleResize);
   }
 
   componentWillUnmount = () => {
     this.stopMarquee();
   }
 
+  start = () => {
+    this.runMarquee();
+  }
+
+  stop = () => {
+    this.stopMarquee();
+  }
+
   renderLandscapeMarquee() {
-    let { loopData } = this.props;
+    let { loopData, hoverPause } = this.props;
 
     return (
-      <div className="marquee-landscape-wrap" ref={(mw) => { this.domMw = mw; }}>
+      <div 
+        className={`marquee-landscape-wrap${hoverPause ? ' hover-pause' : ''}`}
+        ref={(mw) => { this.domMw = mw; }}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
         <div className="marquee-landscape-item" ref={(mi) => { this.domMi = mi; }}>
-          {loopData.map((item, index) => (<div className="marquee-landscape-txt" key={index}>{item.txt}</div>))}
+          {loopData.map((item, index) => (
+            <div 
+              className={`marquee-landscape-txt${hoverPause ? ' hover-pause' : ''}`}
+              key={index}
+              onClick={() => this.handleItemClick(item, index)}
+            >
+              {item.txt}
+            </div>
+          ))}
         </div>
         <div className="marquee-landscape-item">
-          {loopData.map((item, index) => (<div className="marquee-landscape-txt" key={index}>{item.txt}</div>))}
+          {loopData.map((item, index) => (
+            <div 
+              className={`marquee-landscape-txt${hoverPause ? ' hover-pause' : ''}`}
+              key={index}
+              onClick={() => this.handleItemClick(item, index)}
+            >
+              {item.txt}
+            </div>
+          ))}
         </div>
       </div>
     )
   }
 
   renderVerticalMarquee() {
-    let { loopData, verticalItemHeight } = this.props;
+    let { loopData, verticalItemHeight, hoverPause } = this.props;
     return (
-      <div className="marquee-vertical-wrap" ref={(mw) => { this.domMw = mw; }}>
+      <div 
+        className={`marquee-vertical-wrap${hoverPause ? ' hover-pause' : ''}`}
+        ref={(mw) => { this.domMw = mw; }}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
         <div className="marquee-vertical-item" ref={(mi) => { this.domMi = mi; }}>
-          {loopData.map((item, index) => (<div style={{ height: verticalItemHeight, lineHeight: verticalItemHeight }} className="marquee-vertical-txt" key={index}>{item.txt}</div>))}
+          {loopData.map((item, index) => (
+            <div 
+              style={{ height: verticalItemHeight, lineHeight: verticalItemHeight }} 
+              className={`marquee-vertical-txt${hoverPause ? ' hover-pause' : ''}`}
+              key={index}
+              onClick={() => this.handleItemClick(item, index)}
+            >
+              {item.txt}
+            </div>
+          ))}
         </div>
         <div className="marquee-vertical-item">
-          {loopData.map((item, index) => (<div style={{ height: verticalItemHeight, lineHeight: verticalItemHeight }} className="marquee-vertical-txt" key={index}>{item.txt}</div>))}
+          {loopData.map((item, index) => (
+            <div 
+              style={{ height: verticalItemHeight, lineHeight: verticalItemHeight }} 
+              className={`marquee-vertical-txt${hoverPause ? ' hover-pause' : ''}`}
+              key={index}
+              onClick={() => this.handleItemClick(item, index)}
+            >
+              {item.txt}
+            </div>
+          ))}
         </div>
       </div>
     )
   }
 
   render() {
-
     let { direction } = this.props;
 
     return (
@@ -113,7 +213,11 @@ export default class Marquee extends Component {
 Marquee.propTypes = {
   loop: PropTypes.bool,
   loopData: PropTypes.array,
-  getMarquee: PropTypes.func,
   direction: PropTypes.string,
-  verticalItemHeight: PropTypes.string
+  verticalItemHeight: PropTypes.string,
+  speed: PropTypes.number,
+  reverse: PropTypes.bool,         // 是否反向滚动
+  hoverPause: PropTypes.bool,     // 是否启用鼠标移入暂停
+  autoPlay: PropTypes.bool,       // 是否自动开始滚动
+  onClick: PropTypes.func         // 点击项目时的回调
 };
